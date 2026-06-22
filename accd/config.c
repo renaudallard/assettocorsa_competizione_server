73dfe7d3c9c0e6825eadf246a097ccdfed5e2890
Renaud Allard <renaud@allard.it>
2026-06-22 13:08:24 +0200

    config: initialize max_car_slots default before settings.json parse

--- accd/config.c
+++ accd/config.c
@@ -226,6 +226,7 @@ config_load(struct Server *s, const char *cfg_dir)
 	s->tcp_port = 9232;
 	s->udp_port = 9231;
 	s->max_connections = ACC_MAX_CARS;
+	s->max_car_slots = 10;
 	s->lan_discovery = 1;
 	s->password[0] = '\0';
 	s->admin_password[0] = '\0';
